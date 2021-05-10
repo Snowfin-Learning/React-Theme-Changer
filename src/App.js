61482d1831d34a33f1c8f9e52f54d16e7c409c76
Rahul Verma <rahulvermafreelancer@gmail.com>
2021-05-10 15:57:51 +0530

    develop a new light and dark theme function using context API on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ThemeContext from './context/ThemeContext'
+import Header from './components/Header'
+import Hero from './components/HeroSection'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+  const theme = useState('light')
+
+  return(
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Header />
+        <Hero />
+      </div>
+    </ThemeContext.Provider>
+  )
+ 
 }
 
 export default App;
